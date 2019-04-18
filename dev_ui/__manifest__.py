03e57ec7b7cafdf1321158bab989ca57f8c7d685
Adhitya Tri <adhityatriwahyu@gmail.com>
2019-04-18 16:07:31 +0700

    Web Login Style Odoo

---
+++ dev_ui/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Dev_UI",
+    "summary": "DevUI Odoo 10",
+    "version": "0.0.1",
+    "category": "Website",
+    "website": "https://github.com/adhitya_tri",
+    "author": "Adhitya Tri",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/web.xml',
+    ],
+}
